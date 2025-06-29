30b2fe14d9a89fc742f2dc8232da8104f831b325
raulshma <raulshma@gmail.com>
2025-06-29 17:49:19 +0530

    refactor: Update application bootstrap method to use platformBrowserDynamic
    - Replaced the deprecated bootstrapApplication method with platformBrowserDynamic for initializing the Angular application.
    - Simplified the main.ts file by removing unnecessary imports and streamlining the bootstrap process.

--- TechTicker.Frontend/src/main.ts
+++ TechTicker.Frontend/src/main.ts
@@ -1,10 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { importProvidersFrom } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(AppModule)
-  ]
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
